6114840a746b14d32f1b9e750114e60ebe825332
Tejas B <tejasteaj@example.com>
2026-03-06 11:09:36 +0530

    fix: upgrade tailwind to v4 and next.js to fix build errors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.61.161.186', 'localhost:3000'],
 };
 
 export default nextConfig;
